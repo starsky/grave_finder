e1465ecc767a4ad7ba3736b525af6d01d7e33664
Michal Urbaniak <m.urbaniak@gmail.com>
2011-10-03 14:11:01 +0200

    AlertDialog with MapOverlay support in progress

--- src/pl/itiner/map/GraveMap.java
+++ src/pl/itiner/map/GraveMap.java
@@ -6,21 +6,29 @@ import pl.itiner.grave.GeoJSON;
 import pl.itiner.grave.R;
 import pl.itiner.grave.ResultList;
 import pl.itiner.models.Deathman;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.sun.corba.se.impl.interceptors.PINoOpHandlerImpl;
 
 public class GraveMap extends MapActivity {
 	MapView mapView;
@@ -35,6 +43,19 @@ public class GraveMap extends MapActivity {
 	/** Called when the activity is first created. */
 
 	class MapOverlay extends com.google.android.maps.Overlay {
+		
+		private Deathman dt;
+		private Context ctx;
+		private int pin_x;
+		private int pin_y;
+		Point screenPt;
+		Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+				R.drawable.pin);
+		public MapOverlay(Deathman dt, Context ctx)
+		{
+			this.ctx = ctx;
+			this.dt = dt;
+		}
 		@Override
 		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
 				long when) {
@@ -45,11 +66,55 @@ public class GraveMap extends MapActivity {
 			mapView.getProjection().toPixels(p, screenPts);
 
 			// ---add the marker---
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(),
-					R.drawable.pin);
+			screenPt = screenPts;
+			pin_x = screenPts.x;
+			pin_y = screenPts.y - 61;
 			canvas.drawBitmap(bmp, screenPts.x, screenPts.y - 61, null);
 			return true;
 		}
+
+		
+		@Override
+		public boolean onTouchEvent(MotionEvent e, MapView mapView) {
+			// TODO Auto-generated method stub
+			if (e.getAction() == MotionEvent.ACTION_MOVE && e.getAction()!= MotionEvent.ACTION_DOWN)
+			{
+			RectF rec = new RectF();
+			rec.set(bmp.getWidth(),-bmp.getHeight(),bmp.getWidth(),0);
+			rec.offset(pin_x, pin_y+61);
+			if(rec.contains(e.getX(), e.getY()))
+			{
+			//double [] coord = dt.getCoordinates();
+			//Toast.makeText(getApplicationContext(), coord[0] +" "+coord[1], Toast.LENGTH_LONG).show();
+			AlertDialog.Builder alertBox = new AlertDialog.Builder(ctx);
+			alertBox.setMessage("Nawiguj do celu");
+			alertBox.setPositiveButton("Tak", new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					Toast.makeText(getApplicationContext(), "TUTAJ BĘDZIE ROUTING", Toast.LENGTH_LONG).show();
+					dialog.dismiss();
+				}
+			});
+			
+			alertBox.setNegativeButton("Nie teraz", new OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					//Nothing to do
+					dialog.dismiss();
+				}
+			});
+			alertBox.show();
+			}
+			
+			
+		}
+			return true;
+		}
+		
 	}
 
 	@Override
@@ -96,7 +161,7 @@ public class GraveMap extends MapActivity {
 		mc.setZoom(17);
 
 		// ---Add a location marker---
-		MapOverlay mapOverlay = new MapOverlay();
+		MapOverlay mapOverlay = new MapOverlay(tmp,this);
 		List<Overlay> listOfOverlays = mapView.getOverlays();
 		listOfOverlays.clear();
 		listOfOverlays.add(mapOverlay);
