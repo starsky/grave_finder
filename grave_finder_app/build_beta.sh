e681d5d151b8dae08aa0880c16f5944706a81829
Michal Koperski <grave.finder.app@gmail.com>
2013-05-13 19:57:23 +0200

    Obfucating code.

--- grave_finder_app/build_beta.sh
+++ grave_finder_app/build_beta.sh
@@ -1,9 +1,9 @@
 #add proguard rules for beta build
 cat proguard_beta.cfg >> proguard.cfg
-cp build_config_files/values_beta.xml res/build_values.xml
+cp build_config_files/values_beta.xml res/values/build_values.xml
 #run build
 ant -Dkey.store.password=$GF_STORE_PASS -Dkey.alias.password=$GF_ALIAS_PASS -Dkey.store=$GF_KEY_STORE_PATH -Dkey.alias=$KEY_STORE_ALIAS custom-beta-build clean release copy-binaries 
 #Go back to standard proguard rules
 cp proguard_default.cfg proguard.cfg
-cp build_config_files/values_default.xml res/build_values.xml
+cp build_config_files/values_default.xml res/values/build_values.xml
 
--- grave_finder_app/build_release.sh
+++ grave_finder_app/build_release.sh
@@ -1,6 +1,6 @@
 #Ensure standard proguard rules
 cp proguard_default.cfg proguard.cfg
-cp build_config_files/values_release.xml res/build_values.xml
+cp build_config_files/values_release.xml res/values/build_values.xml
 #run build
 ant -Dkey.store.password=$GF_STORE_PASS -Dkey.alias.password=$GF_ALIAS_PASS -Dkey.store=$GF_KEY_STORE_PATH -Dkey.alias=$KEY_STORE_ALIAS custom-production-build clean release copy-binaries
-cp build_config_files/values_default.xml res/build_values.xml
+cp build_config_files/values_default.xml res/values/build_values.xml
